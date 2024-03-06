63852ca077724ed6e1445782206c75373404d41f
Omar <omarkmu@gmail.com>
2024-03-05 19:20:48 -0600

    Refactor and enable overriding

--- SearchPlayersForWeapons/Contents/mods/SearchPlayersForWeapons/media/lua/client/SearchPlayerClient.lua
+++ SearchPlayersForWeapons/Contents/mods/SearchPlayersForWeapons/media/lua/client/SearchPlayerClient.lua
@@ -4,27 +4,37 @@
 --              respondServerItems
 --              respondClientItems
 
-local function predicateWeapon(item)
-	return item:IsWeapon()
-	-- return item:IsWeapon() and item:getActualWeight() >= 1
+SearchPlayer = SearchPlayer or {}
+
+function SearchPlayer.predicateWeapon(item)
+    return item:IsWeapon()
+    -- return item:IsWeapon() and item:getActualWeight() >= 1
+end
+
+function SearchPlayer.reportBeingSearched(player, otherPlayer)
+    player:Say("Being searched by " .. otherPlayer:getDisplayName())
 end
 
 local function OnServerCommand(module, command, args)
-    if module == "SearchPlayer" then
-        if command == "requestClientItems" then
-            local player = getPlayer();
-            local otherPlayer = getPlayerByOnlineID(args[1])
-            local playerInv = player:getInventory();
-            local weapons = playerInv:getAllEval(predicateWeapon);
-            player:Say("Being searched by " .. otherPlayer:getDisplayName())
-            print("Found " .. weapons:size() .. " weapons");
-            if weapons and weapons:size() > 0 then
-                for i=1,weapons:size() do
-                    local item = weapons:get(i-1)
-                    print(item:getName())
-                    tradingUISendAddItem(player, otherPlayer, item);
-                end
-            end
+    if module ~= "SearchPlayer" then
+        return
+    end
+
+    if command == "requestClientItems" then
+        local player = getPlayer();
+        local otherPlayer = getPlayerByOnlineID(args[1])
+        if not otherPlayer then
+            return
+        end
+
+        local playerInv = player:getInventory();
+        local weapons = playerInv:getAllEval(SearchPlayer.predicateWeapon);
+        SearchPlayer.reportBeingSearched(player, otherPlayer)
+        print("Found " .. weapons:size() .. " weapons");
+        for i=0,weapons:size()-1 do
+            local item = weapons:get(i)
+            print(item:getName())
+            tradingUISendAddItem(player, otherPlayer, item);
         end
     end
 end
